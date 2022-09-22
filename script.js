1dfe31b65288b0eb8a6afaa0f4b6d82d99adfe7f
VishavjeetBrar <105453463+VishavjeetBrar@users.noreply.github.com>
2022-09-22 16:07:09 -0400

    complete

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+$(document).ready(function() {
+	$('#salary').on('change', function() {
+	var salary = parseFloat($("#salary").val());
+	console.log(salary);
+	
+	
+	$('#percent').on('change', function() {
+	var percent = parseFloat($("#percent").val());
+	console.log(percent);
+	
+	var spent = salary * percent / 100;
+	spent = spent.toFixed(2);
+	console.log(spent);
+	$('#spend').text('$' + spent);
+	});
+	});
+	});
